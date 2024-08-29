802d4646ad189133d3773f187e2c92e43ae24df0
Trisa Sarifatul Anisak <trisasarifatulanisa@gmail.com>
2024-08-29 21:38:04 +0700

    project java fundamental

---
+++ src/main/java/com/tsa/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.tsa;
+/*
+IntelliJ IDEA 2023.3.1 (Ultimate Edition)
+Build #IU-233.11799.300, built on December 12, 2023
+@Author PT.DIKA a.k.a. Trisa Sarifatul Anisak
+Java Developer
+Created on 29/08/2024 20:14
+@Last Modified 29/08/2024 20:14
+Version 1.0
+*/
+
+public class HelloWorld {
+    public static  String msg;
+    public static void main(String[] args){
+        Main main = new Main();
+        String [] str = "1 2 3".split(" ");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
+}
